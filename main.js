d5c8773cad50f6e053f1b38d7e1c7e136cef0304
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-06-29 15:29:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+Webcam.set({
+
+width:310,
+height:300,
+image_format:"png",
+png_quality:90,
+
+constraints:{
+    facingMode:"environment"//It automatically searches for the REAR camera, else it opens the front camera 
+}
+
+});
+
+var camera=document.getElementById("camera");
+Webcam.attach(camera);
+
+function take_snapshot()
+{
+
+    Webcam.snap(function(data_uri){
+
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+
+    
+}
+console.log("The ml5 version is",ml5.version);
+
+
+classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+
+function modelLoaded()
+{
+    console.log("Model is Loaded");
+}
+
+function check()
+{
+    var img=document.getElementById("captured_image");
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+console.log(results);
+document.getElementById("object_name").innerHTML=results[0].label;
+    }
+}
